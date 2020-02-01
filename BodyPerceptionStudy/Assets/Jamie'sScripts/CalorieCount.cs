90c54a7de6979395047e086253e0df87d5a00b63
jcameraa <ahrtxv@gmail.com>
2020-02-01 11:12:41 -0500

    Added A simple Calorie Counter

---
+++ BodyPerceptionStudy/Assets/Jamie'sScripts/CalorieCount.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalorieCount : MonoBehaviour
+{
+
+
+    public int curCalories;
+
+    private void Start()
+    {
+        curCalories = 1200;
+    }
+
+    public void burnCalories(int calToBurn)
+    {
+
+        curCalories -= calToBurn;
+    }
+
+    public void gainCalories(int calToGain)
+    {
+        curCalories += calToGain;
+
+    }
+
+    public IEnumerator checkKeys()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            //show te calories
+        }
+
+        yield return new WaitForSeconds(3f);
+
+        StartCoroutine(checkKeys());
+
+    }
+}
