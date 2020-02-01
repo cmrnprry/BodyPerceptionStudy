7cc8708c6fb29c31e7016a593516a83d780d5fe3
cmrnprry <perry.ca@husky.neu.edu>
2020-02-01 14:03:11 -0500

    wop

--- BodyPerceptionStudy/Assets/Master Scripts/Meals/ChooseAMeal.cs
+++ BodyPerceptionStudy/Assets/Master Scripts/Meals/ChooseAMeal.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 [System.Serializable]
 public class FoodItem
@@ -13,13 +16,29 @@ public class ChooseAMeal : MonoBehaviour
 {
     // List of foods serialized in the Inspector
     public List<FoodItem> foods;
+    public GameObject button;
+    public GameObject parent;
 
     // TODO AFTER CAM FINISHES UI TASK
     // Do not want multiple canvases. 
     void Start()
     {
-        
+        PopulateList();
     }
+
+    void PopulateList()
+    {
+        foreach (var food in foods)
+        {
+            var choice = Instantiate(button);
+
+            var x = choice.GetComponent<Button>();
+            x.GetComponentInChildren<TextMeshProUGUI>().text = food.name + " - " + food.calories;
+            
+            choice.transform.parent = parent.transform;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
